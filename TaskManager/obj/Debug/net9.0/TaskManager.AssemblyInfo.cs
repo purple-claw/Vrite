830ba56d589563d2c5f4b374d77285fc29f907b3
Purple-Claw <hawkr173@gmail.com>
2025-04-02 10:21:52 +0530

    Initial commit of project files

--- TaskManager/obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
+++ TaskManager/obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d304c4d44ebf57a355088f48869041e0ded1ff4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+965fe734e6f4340d960d3988bfabd4c616f8e21d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
